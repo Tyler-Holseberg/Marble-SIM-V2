ef20f6673daac8842b9b03b65a36482253fd3922
Tyler-Holseberg <tylerholseberg@gmail.com>
2019-02-14 21:03:43 -0500

    changing the marble script and test scene
    edited camerons test scened and the marble script

--- Assets/Scripts/MarbleScript.cs
+++ Assets/Scripts/MarbleScript.cs
@@ -6,7 +6,7 @@ public class MarbleScript : MonoBehaviour
 {
     private Vector3 startPos;
     private Vector3 endPos;
-    private bool scored;
+    public bool scored;
     private Rigidbody marble;
     // Start is called before the first frame update
     void Start()
