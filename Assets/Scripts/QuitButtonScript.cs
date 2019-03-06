1b98710e3677e98549330b0557880a9701f57815
ehilton97 <47570672+ehilton97@users.noreply.github.com>
2019-03-06 18:51:45 -0500

    Added the makings of the menu scene!

---
+++ Assets/Scripts/QuitButtonScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class QuitButtonScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Ball")
+            Application.Quit();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
