4e5bb02abf5772acf77295084c0c54c355ecee6c
duTrip <47576288+duTrip@users.noreply.github.com>
2019-03-11 22:48:18 -0400

    Added Item Spawners

---
+++ Assets/Scripts/SpawnFlatTrack.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnFlatTrack : MonoBehaviour
+{
+    public Object item;
+    private int count;
+    public int max;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        count = GameObject.FindGameObjectsWithTag("Flat").Length;
+        if (count <= max)
+        {
+            Instantiate(item, this.transform);
+        }
+    }
+}
