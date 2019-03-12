bfd06181c29051c7e3430d4eb19075504fb9808b
Timothy <tharri6@g.clemson.edu>
2019-03-12 17:59:16 -0400

    Addition of new scene
    Added a scene that demonstrates the functionaliity of a start and stop button. It features a script that can be used in the actual game to make start and stop buttons during gameplay.

---
+++ Assets/Scripts/StartSimulation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartSimulation : MonoBehaviour
+{
+    public GameObject marble;
+    private Rigidbody marbleRB;
+    private Vector3 marbleStartPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        marbleStartPos = marble.transform.position;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Start Button") 
+        {
+            marble.GetComponent<Rigidbody>().isKinematic = false;
+            marble.GetComponent<Rigidbody>().useGravity = true;
+        }
+
+        if (other.gameObject.name == "Stop Button")
+        {
+            marble.GetComponent<Rigidbody>().isKinematic = true;
+            marble.GetComponent<Rigidbody>().useGravity = false;
+
+            marble.transform.position = marbleStartPos;
+        }
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
