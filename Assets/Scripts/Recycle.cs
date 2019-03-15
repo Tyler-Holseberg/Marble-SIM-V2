69d2e0d55ddff84c58c3e4a4469f8c33feb075fe
Tyler-Holseberg <tylerholseberg@gmail.com>
2019-03-14 23:28:21 -0400

    Script and scene tweaking
    Edited the recycle script and tweaked localavatar positions

--- Assets/Scripts/Recycle.cs
+++ Assets/Scripts/Recycle.cs
@@ -4,36 +4,85 @@ using UnityEngine;
 
 public class Recycle : MonoBehaviour
 {
-    public GameObject track;
-    private Vector3 trackStartPos;
+    public GameObject flat;
+    public GameObject rampBottom;
+    public GameObject RampTop;
+    private Quaternion flatStartRot;
+    private Quaternion bottomStartRot;
+    private Quaternion topStartRot;
+    private GameObject flatTemp;
+    private GameObject rampBottomTemp;
+    private GameObject RampTopTemp;
+
+    
+    
     private float waitTime = 3.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-        trackStartPos = track.transform.position;
+        flatTemp = new GameObject();
+        flatTemp.transform.position = flat.transform.position;
+        flatTemp.transform.rotation = flat.transform.rotation;
+
+        rampBottomTemp = new GameObject();
+        rampBottomTemp.transform.position = rampBottom.transform.position;
+        rampBottomTemp.transform.rotation = rampBottom.transform.rotation;
+
+        RampTopTemp = new GameObject();
+        RampTopTemp.transform.position = RampTop.transform.position;
+        RampTopTemp.transform.rotation = RampTop.transform.rotation;
+        //trackStartPos = track.transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        //tried detecting by tag
-        if (other.name == track.name)
+        
+        //try detecting by tag
+        if (other.gameObject.tag == "Flat")
         {
             Debug.Log("detected collision");
-            StartCoroutine(ResetTrackAfterSeconds(waitTime));
+
+            //StartCoroutine(ResetTrackAfterSeconds(waitTime));
+
+            other.transform.position = flatTemp.transform.position;
+            other.transform.rotation = flatTemp.transform.rotation;
+
         }
+        else if(other.gameObject.tag == "Bottom")
+        {
+            other.transform.position = rampBottomTemp.transform.position;
+            other.transform.rotation = rampBottomTemp.transform.rotation;
+        }
+        else if (other.gameObject.tag == "Top")
+        {
+            other.transform.position = RampTopTemp.transform.position;
+            other.transform.rotation = RampTopTemp.transform.rotation;
+        }
+
     }
+
+
+    /*I dont think we need to use a coroutine for this
 
     IEnumerator ResetTrackAfterSeconds(float seconds) 
     {
         print("WAITING " + waitTime + " SECONDS!");
         yield return new WaitForSeconds(seconds);
-        track.transform.position = trackStartPos;
+        //flat.transform.position = flatTemp.transform.position;
         Debug.Log("whats going on here");
     }
-
+    
+    */
+    
+    
+    
+    
+    
     // Update is called once per frame
     void Update()
     {
     }
+
+
 }
