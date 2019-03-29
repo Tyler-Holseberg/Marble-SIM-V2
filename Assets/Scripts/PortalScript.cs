2118d267f13f3cb510bd16c25dbfa38d7432589c
Tyler-Holseberg <tylerholseberg@gmail.com>
2019-03-29 01:41:59 -0400

    Level 3 Added
    Added a 3rd level that uses a portal mechanic. The user has to build a ramp that gets the marble going a certain speed through the portal. If the speed is enough, it will travel from the exit portal on the robots hand, and into the in the robots other hand.

---
+++ Assets/Scripts/PortalScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    public GameObject marble;
+    public GameObject Portal2;
+    public Vector3 tempVelocity;
+    public Vector3 direction;
+    public GameObject dirTarget;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //change this so this is updated as the portal moves and test in update
+        //Vector3 direction = dirTarget.transform.position - Portal2.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 direction = dirTarget.transform.position - Portal2.transform.position;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Marble")
+        {
+           
+            
+            tempVelocity = marble.GetComponent<Rigidbody>().velocity;
+            marble.GetComponent<Rigidbody>().velocity = (direction.normalized * tempVelocity.magnitude) * 2;
+            marble.transform.position = Portal2.transform.position;
+            // marble.GetComponent<Rigidbody>().angularVelocity = tempVelocity.GetComponent<Rigidbody>().angularVelocity;
+
+
+        }
+    }
+
+
+}
+
