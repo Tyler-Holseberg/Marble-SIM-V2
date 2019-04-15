1a1be0715558c20b65a8244c95ae35e66b0ecfc0
ehilton97 <47570672+ehilton97@users.noreply.github.com>
2019-04-15 18:47:32 -0400

    Erin's first sound script attempt
    I added the sounds and started creating a script to play them on a collision

---
+++ Assets/Scripts/MarbleCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MarbleCollision : MonoBehaviour
+{
+    public AudioSource audioClip;
+
+    void Start()
+    {
+        audioClip = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "table")
+        {
+            audioClip.Play();
+        }
+    }
+}
